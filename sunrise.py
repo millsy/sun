a2fe633a294fb0dd2a3f34b913e19bf730f82d9c
Chris Mills <chris@millsy.me.uk>
2016-10-26 14:16:25 +0100

    Added command line args

--- sunrise.py
+++ sunrise.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python
 
 import ephem
+import sys, getopt
 
-home = ephem.Observer()
-home.horizon = '-12';
-home.lat = '51.9786464'
-home.lon = '-2.2415342'
 
-print(home.previous_rising(ephem.Sun()))
+def main(argv):
+   home = ephem.Observer()
+   lat = '51.9786464'
+   lon = '-2.2415342'
+   horizon = '-6'
+   try:
+      opts, args = getopt.getopt(argv,"ha:o:z:")
+   except getopt.GetoptError:
+      print 'sunrise.py -a lat -o long -z horizon'
+      print 'Example usage: sunrise.py -a 51.9786464 -o -2.2415342 -z 6'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'sunrise.py -a lat -o long -z horizon'
+         print 'Example usage: sunrise.py -a 51.9786464 -o -2.2415342 -z -6'
+         sys.exit()
+      elif opt in ("-a"):
+         lat = arg
+      elif opt in ("-o"):
+         lon = arg
+      elif opt in ("-z"):
+         horizon = arg
+   home.lon = lon
+   home.lat = lat
+   home.horizon = horizon
+   print(home.previous_rising(ephem.Sun()))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
